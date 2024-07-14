9df66c5f9a2ec68d740f794d77dfa9cd96555604
Abhay <abhaydharmale9@gmail.com>
2024-07-14 23:18:00 +0530

    page started

--- script.js
+++ script.js
@@ -37,3 +37,25 @@ function closebtn() {
 
 menubtn.addEventListener('click', runmenu);
 close.addEventListener('click', closebtn);
+
+
+const floatingImages = () =>{
+    let section = document.querySelector(".max-cont")
+    section.addEventListener("mousemove", function(details){
+        document.querySelectorAll(".image").forEach((elem)=>{
+            const position = elem.getAttribute("value")
+    
+            var x = (window.innerWidth + details.clientX * position)/150
+            var y = (window.innerHeight + details.clientY * position)/150
+    
+            // elem.style.transform = `translateX(${x}px) translateY(${y}px)`
+            gsap.to(elem, {
+                x: x,
+                y: y,
+                // duration: 0.01,
+                ease: "power4.out"
+            })
+        })
+    })
+}
+floatingImages()
